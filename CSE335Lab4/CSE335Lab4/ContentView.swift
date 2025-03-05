602fe10161b989e216a38d6989370f8d890c84fc
bgseavoy3 <89864854+bgseavoy3@users.noreply.github.com>
2025-03-04 17:40:49 -0700

    Commit 2 - Started Frontend
    Created basic layout for frontend, still need to add multiple functionality aspects and fix ZStack in middle of page

--- CSE335Lab4/CSE335Lab4/ContentView.swift
+++ CSE335Lab4/CSE335Lab4/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = BookViewModel(inputBooksL [])
+    @StateObject var bookInc: Int = 0;
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavicationView
+        {
+            .navigationTitle("BookList")
+            .toolbar
+            {
+                ToolbarItem(placement: navigationBarTrailing)
+                {
+                    Button("Search",action: 
+                          {
+                              //bring up search interface
+                          })
+                }
+                ToolBarItem(placement: .bottomBar)
+                {
+                    HStack
+                    {
+                        Button("Add", action:
+                              {
+                                  //bring up add interface
+                              })
+                        Button("Delete", action:
+                              {
+                                  //bring up delete interface
+                              })
+                        .padding()
+                        Button("Previous", action:
+                              {
+                                  //bring up previous book
+                              })
+                        Button("Next", action:
+                              {
+                                  //bring up next book
+                              })
+                    }
+            }
+            VStack 
+            {
+                if(viewModel.books.isEmpty())
+                {
+                    Text("Preview Unavailable")
+                    Text("Please Input a Book To Access Preview")
+                }
+                else
+                {
+                    ZStack
+                    {
+                        Rectangle() // edit to make correct size
+                        Text("Title: " + viewModel.books[bookInc].title)//make sure it is in middle of rectangle
+                        Text("Author: " + viewModel.books[bookInc].author)
+                        Text("Genre: " + viewModel.books[bookInc].genre)
+                        Text("Price: " + viewModel.books[bookInc].price)
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
